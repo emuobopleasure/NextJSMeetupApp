b3db0aa97ed42b39bb4f411435ce015b2e65e392
Davis06 <davis06@outlook.com>
2024-08-19 19:19:49 +0100

    Added a custom MeetupDetail component and applied CSS Modules for styling

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://www.meetup.com/blog/wp-content/uploads/2022/05/option-1.jpg"
+      title="first title"
+      address="some street 5, some city"
+      description="The meetup description"
+    />
+  );
+};
+
+export default MeetupDetails;
